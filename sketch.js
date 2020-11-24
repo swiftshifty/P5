e01cc478552c080302f6df18a0f2c9825feedef1
swiftshifty <74018616+swiftshifty@users.noreply.github.com>
2020-11-24 16:56:00 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    // put setup code here
+  createCanvas(500,500);
+  background(color(0, 0, 255));
+}
+
+
+
+
+function draw() {
+  // put drawing code here
+    // p5 Color object
+stroke(color(0, 0, 255));
+strokeWeight(4);
+rect(20, 20, 60, 60);
+    circle(30, 30, 20);
+}
